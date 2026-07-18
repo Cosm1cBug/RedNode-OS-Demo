696512ca5503abb2b2e1dda3b31da8ec3ea359b7
COSMICBUG <54317137+Cosm1cBug@users.noreply.github.com>
2026-07-19 01:00:32 +0530

    .

--- os/nixos/flake.nix
+++ os/nixos/flake.nix
@@ -2,7 +2,7 @@
   description = "RedNode-OS – Personal Autonomous Operating System – Privacy-first, self-aware, sentient";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
